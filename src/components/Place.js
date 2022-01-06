e45f2a3c81f3270a822f0485f4d3e101b323ca8c
Simon K <simonk9@outlook.com>
2022-01-05 21:13:03 -0500

    edit width mobile

--- src/components/Place.js
+++ src/components/Place.js
@@ -10,7 +10,7 @@ function Place(x){
             <div className="click-box">
                 <i class="marker fas fa-map-marker-alt"></i>
                 <span className="country">{x.item.country}</span>
-                <a href={`${x.item.map}`}><span className="map">View on Google Maps</span></a>
+                <a href={`${x.item.map}`}><span className="map">View on Google</span></a>
             </div>
             
             <p>
